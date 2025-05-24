9f36db2c3812e8c4aa3d35d84e5d0fb8995545bc
mohammad-k13 <mohammadkeshavarz.a.13@gmail.com>
2025-05-24 09:35:13 +0330

    done counter test

--- __test__/components/counter.test.tsx
+++ __test__/components/counter.test.tsx
@@ -1,13 +1,29 @@
 import AppCounter from "@/components/app-counter";
+import userEvent from "@testing-library/user-event";
 import { render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
+import { type ReactNode } from "react";
+
+function setup(component: ReactNode) {
+      return {
+            user: userEvent.setup(),
+            component: render(component),
+      };
+}
 
 describe("Counter", () => {
-      it("Should render the counter component", () => {
+      it("Should render the counter component and show count: 0", () => {
             const screen = render(<AppCounter />);
             const button = screen.getByRole("button");
+
             expect(button).toBeInTheDocument();
-            expect(button.textContent).toBe(/Count:/);
+            expect(button).toHaveTextContent(/Count: 0/);
       });
-      it("Should render increament count", () => {});
+
+      it("Should render increament count", async () => {
+            const { user, component } = setup(<AppCounter />);
+
+            await user.click(component.getByRole("button"));
+            expect(component.getByRole("button")).toHaveTextContent(/Count: 1/);
+      });
 });
