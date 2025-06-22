6d52cfd98a0407e0b0f720b9c0fba92dceef36d1
mohammad-k13 <mohammadkeshavarz.a.13@gmail.com>
2025-06-22 19:46:34 +0330

    signupScheme tested

---
+++ __test__/units/utils/formSchema/signup-schem.test.ts
@@ -0,0 +1,91 @@
+import signupSchema, { signupSchemaErrors } from '@/utils/form-schema/signup-schema';
+import { describe, expect, it } from 'vitest';
+import { z } from 'zod';
+
+type signupDataType = z.infer<typeof signupSchema>;
+describe('SignupSchema', () => {
+      const validData: signupDataType = {
+            username: 'mohammadk13',
+            email: 'mohammadk13@gmail.com',
+            password: 'mohammadk13',
+      };
+
+      const invalidData: signupDataType = {
+            username: '2',
+            password: '234',
+            email: 'mohammad',
+      };
+
+      const emptyData: signupDataType = {
+            username: '',
+            password: '',
+            email: '',
+      };
+
+      const getVlidationError = (data: signupDataType): signupDataType & { success: boolean } => {
+            const { success, error } = signupSchema.safeParse(data);
+
+            const validationErrors = error?.flatten().fieldErrors;
+
+            return {
+                  username: validationErrors?.username?.[0] || '',
+                  email: validationErrors?.email?.[0] || '',
+                  password: validationErrors?.password?.[0] || '',
+                  success,
+            };
+      };
+
+      it('Should reject empty data with currect message', () => {
+            const { email, password, username, success } = getVlidationError(emptyData);
+
+            expect(success).toBe(false);
+
+            expect(username).toBe(signupSchemaErrors.username.invalidUsername);
+            expect(email).toBe(signupSchemaErrors.email.invalidEmail);
+            expect(password).toBe(signupSchemaErrors.password.invalidPassword);
+      });
+
+      it('Should reject username more than 20 characters with currect message', () => {
+            const { username, success } = getVlidationError({
+                  username: '12341234123412341234omsdflf',
+                  email: '',
+                  password: '',
+            });
+
+            expect(success).toBe(false);
+            expect(username).toBe(signupSchemaErrors.username.longUsername);
+      });
+
+      it('Should reject username inclucde special character with currect message', () => {
+            const { username, success } = getVlidationError({
+                  username: 'asldkjfsa@#$',
+                  email: '',
+                  password: '',
+            });
+
+            expect(success).toBe(false);
+            expect(username).toBe(signupSchemaErrors.username.invalidUsername);
+      });
+
+      it('Should reject invalid email with currect message', () => {
+            const { email, success } = getVlidationError({
+                  username: validData.username,
+                  email: invalidData.email,
+                  password: validData.password,
+            });
+
+            expect(success).toBe(false);
+            expect(email).toBe(signupSchemaErrors.email.invalidEmail);
+      });
+
+      it('Should reject short password with currect message', () => {
+            const { email, success } = getVlidationError({
+                  username: validData.username,
+                  email: validData.email,
+                  password: invalidData.password,
+            });
+
+            expect(success).toBe(false);
+            expect(email).toBe(signupSchemaErrors.password.invalidPassword);
+      });
+});
--- utils/form-schema/signup-schema.ts
+++ utils/form-schema/signup-schema.ts
@@ -1,15 +1,29 @@
 import { z } from 'zod';
 
+type SignupSchemaErrorsType = Record<keyof z.infer<typeof signupSchema>, Record<string, string>>;
+export const signupSchemaErrors = {
+      username: {
+            shortUsername: 'Username must be at least 3 characters',
+            longUsername: 'Username must be at most 20 characters',
+            invalidUsername: 'Username can only contain letters, numbers, and underscores',
+      },
+      email: {
+            invalidEmail: 'Invalid email address',
+      },
+      password: {
+            invalidPassword: 'Password must be at least 6 characters',
+      },
+};
 const signupSchema = z.object({
       username: z
             .string()
-            .min(3, { message: 'Username must be at least 3 characters' })
-            .max(20, { message: 'Username must be at most 20 characters' })
+            .min(3, { message: signupSchemaErrors.username.shortUsername })
+            .max(20, { message: signupSchemaErrors.username.longUsername })
             .regex(/^[a-zA-Z0-9_]+$/, {
-                  message: 'Username can only contain letters, numbers, and underscores',
+                  message: signupSchemaErrors.username.invalidUsername,
             }),
-      email: z.string().email({ message: 'Invalid email address' }),
-      password: z.string().min(6, { message: 'Password must be at least 6 characters' }),
+      email: z.string().email({ message: signupSchemaErrors.email.invalidEmail }),
+      password: z.string().min(6, { message: signupSchemaErrors.password.invalidPassword }),
 });
 
 export default signupSchema;
